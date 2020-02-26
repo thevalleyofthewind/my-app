76057da4244778eb6b60ebc3541549d5db1de407
thevalleyofthewind <thevalleyofthewind.channel@gmail.com>
2020-02-26 11:37:26 +0800

    Updates

---
+++ precache-manifest.03982e95afb297c10729c48aa9b20aa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a20be5e1f3cec431b434",
+    "url": "/my-app/static/css/main.dce1fa78.chunk.css"
+  },
+  {
+    "revision": "a20be5e1f3cec431b434",
+    "url": "/my-app/static/js/main.a20be5e1.chunk.js"
+  },
+  {
+    "revision": "caff8f4450a1e5b1c09b",
+    "url": "/my-app/static/js/runtime~main.caff8f44.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/my-app/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "04a3fae88ed1b8bd36a6e8a027fb9264",
+    "url": "/my-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.9cb25716dc2d2d8e86f2a16b9058dd7f.js"
+  "/my-app/precache-manifest.03982e95afb297c10729c48aa9b20aa5.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute("/my-app/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
