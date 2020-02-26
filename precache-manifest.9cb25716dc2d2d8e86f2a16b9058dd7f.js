fd87fec3289847ab2a911e30110852a68191bdb7
thevalleyofthewind <thevalleyofthewind.channel@gmail.com>
2020-02-26 11:25:50 +0800

    Updates

---
+++ precache-manifest.9cb25716dc2d2d8e86f2a16b9058dd7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "425f72ec5c31aa79cd2f8af5c439d5b0",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "67a78ae040247f41e5ae",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "241abb0a83e753575532",
+    "url": "/my-app/static/js/2.bcc72c2a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-app/static/js/2.bcc72c2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a78ae040247f41e5ae",
+    "url": "/my-app/static/js/main.0c893ca3.chunk.js"
+  },
+  {
+    "revision": "0c10fa8248c25a2f1677",
+    "url": "/my-app/static/js/runtime-main.dd12a99c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
